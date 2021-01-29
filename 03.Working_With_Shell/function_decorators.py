3d2873b2edb6845bdf83539fb5b260d0abc09b44
UbuntuDev_RaiZy <UbuntuDev_RaiZy>
2021-01-29 15:01:08 +0100

    Adding function_decorators

---
+++ 03.Working_With_Shell/function_decorators.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+ 
+def some_decorator(wrapped_function):
+    def wrapped():
+        print('Do something before calling wrapped function')
+        wrapped_function()
+        print('Do something after calling wrapped function')
+    return wrapped
+
+def foobat():
+    print('foobat')
+
+@some_decorator
+def batfoo():
+    print('batfoo')
+
+if __name__ == '__main__':
+    f = some_decorator(foobat)
+    # print(f())
+    f()
+    batfoo()
